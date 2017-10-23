153aea9864379205d78fdc28fed295c8aabc45e2
Zaynaib <zaynaibg@gmail.com>
2017-10-23 16:19:23 -0500

    finished first part of assignment

--- bamazon.sql
+++ bamazon.sql
@@ -8,13 +8,14 @@ CREATE TABLE products (
     product_name varchar(255),
     department_name varchar(255),
     price DECIMAL(20,2)	,
-    stock_quantity INTEGER(11) 
+    stock_quantity INTEGER(11), 
     PRIMARY KEY (item_id)
 );
 
 
 INSERT INTO products (item_id, product_name, department_name,price,stock_quantity)
-VALUES ("vanilla ice-cream", "Dairy", 2.50,10), ("chocolate ice-cream", "Dairy,"3.10, 120), ("strawberry ice-cream","Dairy" ,3.25,2),
-("socks", "Apparel", 5.50,2),("underwear", "Apparel", 10.50,50),("laptop", "Electronics", 500.00,80)
-,("iPhone", "Electornics", 900.85,50),("iPod", "Electornics", 500.00,2),("Dawn Soap", "Household", 2.50,100),
-("pusheen", "toys", 20.50,25)
+VALUES (1,"vanilla ice-cream", "Dairy", 2.50,10), (2,"chocolate ice-cream", "Dairy",3.10, 120), (3,"strawberry ice-cream","Dairy" ,3.25,2),
+(4,"socks", "Apparel", 5.50,2),(5,"underwear", "Apparel", 10.50,50),(6,"laptop", "Electronics", 500.00,80)
+,(7,"iPhone", "Electornics", 900.85,50),(8,"iPod", "Electornics", 500.00,2),(9,"Dawn Soap", "Household", 2.50,100),
+(10,"pusheen", "toys", 20.50,25);
+
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,48 +1,38 @@
 //require the node.js sql library
 var mysql = require("mysql");
 
+//require the inquirer 
+var inquirer = require("inquirer");
 
 //configuration information of database
-var config ={
+var connection = mysql.createConnection({
 	host:"localhost",
 	port:3306,
 	user: "root",
-	password:"",
+	password:"1234",
 	database:"bamazon"
-}
+});
 
 //connect to database
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
   //queryAllSongs();
-  //queryDanceSongs();
+  displayQuery();
+   
 });
 
+  
 
-//how to end connection
-//connection.end();
 
 //create some CRUD functions (create read update delete)
 
 /*
-
-
 Running this application will first display all of the items available for sale. 
 Include the ids, names, and prices of products for sale.
-
 */
 
-function queryAllSongs() {
-  connection.query("SELECT * FROM songs", function(err, res) {
-    for (var i = 0; i < res.length; i++) {
-      console.log(res[i].id + " | " + res[i].title + " | " + res[i].artist + " | " + res[i].genre);
-    }
-    console.log("-----------------------------------");
-  });
-}
-
- (item_id, product_name, department_name,price,stock_quantity)
+//(item_id, product_name, department_name,price,stock_quantity)
 //work on display functionality
 function displayQuery(){
 	connection.query("SELECT * FROM products", function(err,res){
@@ -51,26 +41,99 @@ function displayQuery(){
 			console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].price + " | " + res[i].stock_quantity);
 		}
 		console.log("-----------------------------------");
-		connection.end();
+    question();
+		//connection.end(); //end connection after established
 
 	});//end of displayQuery
-	
 }
 
+//this function prompts the user to ask a letter
+function question(){
+inquirer.prompt([{
+    type: "input",
+    name: "idSelect",
+    message: "What's the ID of the product you would like to buy?"
+  },
+  {
+    type: "input",
+    name: "unitSelect",
+    message: "How many units of the product you would like to buy?"
+  }]).then( function (response) {
+      var prodId = parseInt(response.idSelect);
+      var units = response.unitSelect;
+    
+      //console.log(response.idSelect);
+      //console.log(response.unitSelect);
+      placeOrder(prodId,units);
+
+  })
+};
+
 /*
-6. The app should then prompt users with two messages.
-
-   * The first should ask them the ID of the product they would like to buy.
-   * The second message should ask how many units of the product they would like to buy.
-
-
-
-7. Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
-
-   * If not, the app should log a phrase like `Insufficient quantity!`, and then prevent the order from going through.
-
-8. However, if your store _does_ have enough of the product, you should fulfill the customer's order.
-   * This means updating the SQL database to reflect the remaining quantity.
-   * Once the update goes through, show the customer the total cost of their purchase.
-
+Once the customer has placed the order, your application should check if your 
+store has enough of the product to meet the customer's request.
 */
+
+function placeOrder(productId,numUnits) {
+  console.log("Customer is placing an order...\n");
+  var query = connection.query(
+    "SELECT stock_quantity FROM products WHERE item_id=?",
+    [productId],
+    function(err, res) {
+      if(err){
+        console.log(err)
+      }
+      console.log(res);
+      
+      var quantity = res[0].stock_quantity;
+      if(parseInt(quantity)<numUnits){
+        console.log("we are out of stock");
+      }else{
+        console.log("This item available");
+        updateProduct(productId,numUnits,quantity);
+      }
+      
+      //console.log(quantity);
+      
+    }
+
+    
+  ); //end of query
+
+  // logs the actual query being run
+  console.log(query.sql);
+};
+
+
+//updating products
+function updateProduct(id,customerUnits,currentQuantity) {
+  console.log("Updating quantity...\n");
+  var updatedQuantity = currentQuantity - customerUnits;
+  console.log(updatedQuantity);
+  
+  var query = connection.query(
+    "UPDATE products SET ? WHERE ?",
+    [{
+        stock_quantity:updatedQuantity
+        //stock_quantity:118
+      },
+      {
+        item_id:id
+      }
+    ],
+    function(err, res) {
+      //console.log(res.affectedRows);
+      console.log(res.affectedRows);
+    });
+
+  // logs the actual query being run
+  console.log(query.sql);
+  connection.end();//be careful with connection end statements
+
+}
+
+
+
+
+
+
